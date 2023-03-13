c03110d1b0f6385d895548f136f8e1c94f63d741
MoneyStats <giovannilamarmora.working@gmail.com>
2023-03-13 16:05:28 +0000

    Update from https://github.com/MoneyStats/Angular-MoneyStats-Application/commit/f0979653279694c279b49d7dfec94b04711fc7ca

--- assets/core/interceptors/error.service.ts
+++ assets/core/interceptors/error.service.ts
@@ -13,7 +13,9 @@ export class ErrorService {
   constructor(private http: HttpClient) {}
 
   getError(error: any) {
-    if (error.error.error) {
+    console.log('getError');
+    if (error.error.error != null) {
+      console.log('getError != null');
       this.exception = error.error;
       this.exception.error!.statusCode = error.status;
     } else {
@@ -22,6 +24,7 @@ export class ErrorService {
   }
 
   getUnknowError(error: any) {
+    console.log('getError = null');
     this.exception.url = error.url;
     this.exception.dateTime = new Date();
     let errorModel: Error = new Error();
@@ -30,5 +33,6 @@ export class ErrorService {
     errorModel.statusCode = HttpStatusCode.InternalServerError;
     errorModel.exceptionName = error.statusText;
     this.exception.error = errorModel;
+    console.log(this.exception);
   }
 }
